f9780759ac6fef1af52f8c6d4276305ca4e18d98
iMaple <imaple.org@gmail.com>
2022-05-09 01:45:29 +0800

    v1.0.0

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install upx -y
+ln -s /usr/bin/upx staging_dir/host/bin/
+
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
+echo "src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
+
+./scripts/feeds update -a
+./scripts/feeds install luci-app-passwall
+
+make defconfig
+make menuconfig
+
+make download -j8 V=s
+make package/luci-app-passwall/compile V=99
+#make package/luci-app-passwall/{clean,compile} -j4
+make package/index
