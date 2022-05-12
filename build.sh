7f92d26d92975c01a128a4d8e79369163ca2f54b
iMaple <imaple.org@gmail.com>
2022-05-12 11:43:02 +0800

    v1.1.3

--- build.sh
+++ build.sh
@@ -6,9 +6,11 @@ ln -s /usr/bin/upx staging_dir/host/bin/
 
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
 echo "src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
+echo "src-git passwall2_luci https://github.com/xiaorouji/openwrt-passwall2.git" >> feeds.conf.default
 
 ./scripts/feeds update -a
 ./scripts/feeds install luci-app-passwall
+./scripts/feeds install luci-app-passwall2
 
 make defconfig
 #make menuconfig
